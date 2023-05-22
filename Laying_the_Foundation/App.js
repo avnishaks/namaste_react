25f991f922a446efb103c944116ed80454b0b355
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-05-22 09:49:06 +0530

    Babel Transpile the React CreateElement code and Power of Babel in compilation to help browser by compiling the code

--- Laying_the_Foundation/App.js
+++ Laying_the_Foundation/App.js
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 
 const head=React.createElement("h1",{id:"heading"},"Abhi bhor bhayo");
 
+console.log(head)
+
+// JSX : HTML-like or XML-like Syntax
+//JSX => Babel transpiles it to React.createElement => ReactElement -JS object => HTMLElement (render)
+
+const jsxheading=<h1 className="head" tabIndex="1">Namaste React using JSX</h1>
+
+console.log(jsxheading)
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(head);
+root.render(jsxheading);
+
