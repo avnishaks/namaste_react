f1eea93bc9573ad3e58fdb98f67ba72890f0b230
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-05-22 12:08:59 +0530

    Fundamental of JSX with React Element and Component

--- Laying_the_Foundation/App.js
+++ Laying_the_Foundation/App.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 // React Functional Component
 
+const shape = <h1>Its a new shape and formation</h1>;
+
 const Tittle = () => (
   <h1 className="title" tabIndex="1">
     Namaste React using Tittle Component
@@ -10,12 +12,16 @@ const Tittle = () => (
 );
 
 const HeadingComponent = () => (
-  <div id="container">
-    <Tittle />
-    <h1 className="head" tabIndex="1">
-      Namaste React using Functional Component
-    </h1>
-  </div>
+  <>
+    <div id="container">
+      {shape}
+      <Tittle />
+      <h1 className="head" tabIndex="1">
+        Namaste React using Functional Component
+      </h1>
+    </div>
+    <div id="container2"></div>
+  </>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
