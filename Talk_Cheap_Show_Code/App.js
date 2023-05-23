dc47b41ad79f959b4021cd1116d512a680c55c15
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-05-23 08:58:27 +0530

    App Design Overview

--- Talk_Cheap_Show_Code/App.js
+++ Talk_Cheap_Show_Code/App.js
@@ -8,6 +8,30 @@ const Tittle =()=>(
   </h1>
 );
 
+const AppLayout =()=>{
+  return(
+  {
+    /*
+    Header
+      - Logo
+      - Nav iteam (Right side)
+      - Cart  
+    Body Component
+      -Search Bar
+      -Resturant List
+        -Resturant cards 
+            - Image
+            - Name 
+            - Rating 
+            - Cusines
+     Footer
+      - link
+      - Copyright
+    */
+  }
+)
+}
+
 const HeadingComponent = () => (
   <>
     <div id="container">
@@ -18,14 +42,8 @@ const HeadingComponent = () => (
     </div>
   </>
 );
+  
 
-
-
-const AppLayout =()=>(
-  <h1 className="title" key="h1">
-   Food Villa
-  </h1>
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent />);
