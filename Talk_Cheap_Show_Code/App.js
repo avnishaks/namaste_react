208ec78b5b009dcde6de068c3a3fc2f264129532
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-05-24 10:08:30 +0530

    Restro Cards is added

--- Talk_Cheap_Show_Code/App.js
+++ Talk_Cheap_Show_Code/App.js
@@ -9,10 +9,10 @@ const Tittle = () => (
   />
 );
 
-const Header = () => (
-  <>
+const Header = () => {
+  return (
     <div className="header">
-      {Tittle()}
+     <Tittle/>
       <div className="nav-items">
         <ul>
           <li>Home</li>
@@ -22,11 +22,45 @@ const Header = () => (
         </ul>
       </div>
     </div>
-  </>
-);
+  )
+};
+
+const BurgerKing={
+  name:"BurgerKing",
+  img:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/0dca660dbdf2e04f9b861c4426ffd41e",
+  cousines: ["Beverages","Bakery","Fast Food"],
+  ratings: "4.2"
+}
+
+const RestrauntCard = () => {
+  return (
+    <div className="card">
+       <img src={BurgerKing.img} />
+       <h5>{BurgerKing.name}</h5>
+       <h6>{BurgerKing.cousines.join(",")}</h6>
+       <h7>{BurgerKing.ratings} stars</h7>
+
+    </div>
+  );
+};
 
 const Body = () => {
-  return <h4> BodyComponent </h4>;
+  return (
+    <div className="restro-list">
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+      <RestrauntCard/>
+    </div>
+  );
 };
 const Footer = () => {
   return <h4> FooterComponent </h4>;
